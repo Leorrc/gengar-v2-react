a4fdfb6af39f899cf90d5a5c53705abee39f78ba
Leonardo Carvalho <leorrc1@gmail.com>
2024-05-21 14:17:32 -0300

    update: adapted mobile

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -40,95 +40,96 @@ export function SignUp() {
 
   return (
     <>
-      {isLoading ? (<Loading />) : (<section className="bg-shape2 flex items-center justify-center h-screen w-screen p-4">
-        <motion.div
-          initial={{ x: '13%' }}
-          animate={{ x: '0' }}
-          className="HeaderColor lg:w-[50%] h-full py-10 px-0 rounded"></motion.div>
+      {isLoading ? (<Loading />) : (
+        <section className="bg-shape2 overflow-hidden flex items-center justify-center h-screen w-screen p-4">
+          <motion.div
+            initial={{ x: '13%' }}
+            animate={{ x: '0' }}
+            className="HeaderColor lg:w-[50%] lg:h-full py-10 px-0 rounded"></motion.div>
 
-        <motion.div
-          initial={{ x: '-13%' }}
-          animate={{ x: '0' }}
-          className="lg:w-[50%] max-h-[56.25rem] py-10 px-0 md:p-12">
-          <header className="flex flex-col items-center">
-            <img
-              className="animate-[gengar_2s_ease-in-out_infinite] drop-shadow-[0_0_1rem_rgb(36,15,70)] mx-auto w-auto h-auto"
-              src={img}
-              alt="gengar"
-            />
+          <motion.div
+            initial={{ x: '-13%' }}
+            animate={{ x: '0' }}
+            className="lg:w-[50%] max-h-[56.25rem] py-10 px-0 md:p-12">
+            <header className="flex flex-col items-center">
+              <img
+                className="animate-[gengar_2s_ease-in-out_infinite] drop-shadow-[0_0_1rem_rgb(36,15,70)] mx-auto w-auto h-auto"
+                src={img}
+                alt="gengar"
+              />
 
-            <header className="mt-4 text-lg text-gray-100">
-              Crie uma nova conta
+              <header className="mt-0 text-lg text-gray-100">
+                Crie uma nova conta
+              </header>
+
+              <h2 className="text-gray-400 mt-1">Faça seu cadastro!</h2>
             </header>
 
-            <h2 className="text-gray-400 mt-1">Faça seu cadastro!</h2>
-          </header>
+            <form className="flex flex-col mt-10 my-0 mx-auto max-w-sm w-full gap-4" onSubmit={handleSubmit(handleSignUp)}>
+              <div className="flex flex-col gap-2">
+                <label className="font-semibold text-gray-100 ">Seu nome</label>
 
-          <form className="flex flex-col mt-10 my-0 mx-auto max-w-sm w-full gap-4" onSubmit={handleSubmit(handleSignUp)}>
-            <div className="flex flex-col gap-2">
-              <label className="font-semibold text-gray-100 ">Seu nome</label>
-
-              <input
-                className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300 text-gray-100"
-                {...register('name')}
-                name='name'
-                type="text"
-                placeholder="Digite seu nome"
-              />
-              {errors.name && (<span className='text-red font-medium font-sans'> {errors.name ? errors.name.message : 'Digite o nome do usuário'} </span>)}
-            </div>
-            <div className="flex flex-col gap-2">
-              <label className="font-semibold text-gray-100 ">Seu e-mail</label>
-
-              <input
-                className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300 text-gray-100"
-                {...register('email')}
-                name='email'
-                type="email"
-                placeholder="Digite seu e-mail"
-              />
-              {errors.email && (<span className='text-red font-medium font-sans'> {errors.email ? errors.email.message : 'Digite o nome do usuário'} </span>)}
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <label className="font-semibold text-gray-100 ">Sua senha</label>
-
-              <input
-                className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300 text-gray-100"
-                {...register('password')}
-                name='password'
-                type="password"
-                placeholder="Digite sua senha"
-              />
-              {errors.password && (<span className='text-red font-medium font-sans'> {errors.password?.message} </span>)}
-            </div>
-
-            {isSubmitting ? (
-              <Spinner />
-            ) : (
-              <div className="mt-4 flex flex-col gap-4 ">
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="justify-center items-center text-center text-white py-3 px-4 bg-roxo rounded font-semibold text-sm  transition-colors hover:bg-roxo2 focus:ring-2 ring-white">
-                  Cadastrar
-                </button>
+                <input
+                  className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300 text-gray-100"
+                  {...register('name')}
+                  name='name'
+                  type="text"
+                  placeholder="Digite seu nome"
+                />
+                {errors.name && (<span className='text-red font-medium font-sans'> {errors.name ? errors.name.message : 'Digite o nome do usuário'} </span>)}
               </div>
-            )}
-            <div className="pt-8">
-              <p className="font-normal text-sm text-gray-100 ">
-                Já possui uma conta?{' '}
-                <Link to="/"
+              <div className="flex flex-col gap-2">
+                <label className="font-semibold text-gray-100 ">Seu e-mail</label>
 
-                  className="text-roxo2 font-bold hover:underline transition-all">
-                  {' '}
-                  Clique aqui
-                </Link>
-              </p>
-            </div>
-          </form>
-        </motion.div>
-      </section>)}
+                <input
+                  className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300 text-gray-100"
+                  {...register('email')}
+                  name='email'
+                  type="email"
+                  placeholder="Digite seu e-mail"
+                />
+                {errors.email && (<span className='text-red font-medium font-sans'> {errors.email ? errors.email.message : 'Digite o nome do usuário'} </span>)}
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label className="font-semibold text-gray-100 ">Sua senha</label>
+
+                <input
+                  className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300 text-gray-100"
+                  {...register('password')}
+                  name='password'
+                  type="password"
+                  placeholder="Digite sua senha"
+                />
+                {errors.password && (<span className='text-red font-medium font-sans'> {errors.password?.message} </span>)}
+              </div>
+
+              {isSubmitting ? (
+                <Spinner />
+              ) : (
+                <div className="mt-4 flex flex-col gap-4 ">
+                  <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className="justify-center items-center text-center text-white py-3 px-4 bg-roxo rounded font-semibold text-sm  transition-colors hover:bg-roxo2 focus:ring-2 ring-white">
+                    Cadastrar
+                  </button>
+                </div>
+              )}
+              <div className="pt-8">
+                <p className="font-normal text-sm text-gray-100 ">
+                  Já possui uma conta?{' '}
+                  <Link to="/"
+
+                    className="text-roxo2 font-bold hover:underline transition-all">
+                    {' '}
+                    Clique aqui
+                  </Link>
+                </p>
+              </div>
+            </form>
+          </motion.div>
+        </section>)}
     </>
   )
 }
